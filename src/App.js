bad561056d5f4cd4aed7f31149d4a6942b827d24
Daniel Hartley <Daniel_Hartley@tcenergy.com>
2021-12-16 11:24:19 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "react-bootstrap";
+import navbar from './navbar'
+import { Button } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+        <Button variant='success' onClick={() => console.log("click")}>Click me</Button>{' '}
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+        <Button variant="link">Link</Button>
+
+        <Navbar />
+
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const name = <h1> Daniel Hartley </h1>
+const googleImg = <img alt='chaf' src="C:\Users\Daniel_Hartley\Projects\react\daniel-test-app\src\pictures\AlternateRealityChad.png" />
+
+
+ReactDOM.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  googleImg,
+  document.getElementById('root'),
+);
+
+
