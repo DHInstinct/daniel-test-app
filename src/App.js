a72b78385a3e69ba7093f1268951117322919669
Daniel Hartley <Daniel_Hartley@tcenergy.com>
2021-12-16 14:37:44 -0500

    end of day 12-16-21

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import { Navbar } from "react-bootstrap";
 import navbar from './navbar'
-import { Button } from 'react-bootstrap'
+import { Button, Container, Row, Alert, Breadcrumb } from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
+      <div className='App'>
+        <Container>
+          <Alert variant='primary'>These are buttons</Alert>
+            <Breadcrumb>
+              <Breadcrumb.item>Test1</Breadcrumb.item>
+              <Breadcrumb.item>Test2</Breadcrumb.item>
+              <Breadcrumb.item active >Test3</Breadcrumb.item>
+            </Breadcrumb>
+          <Row>
+            <Button variant='success' onClick={() => console.log("click")}>Click me</Button>{' '}
+            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="secondary">Secondary</Button>{' '}
+          </Row>
 
-        <Button variant='success' onClick={() => console.log("click")}>Click me</Button>{' '}
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-        <Button variant="link">Link</Button>
+        <Row>
+          <Button variant="success">Success</Button>{' '}
+          <Button variant="warning">Warning</Button>{' '}
+          <Button variant="danger">Danger</Button> {' '}
+          <Button variant="info">Info</Button>{' '}
+        </Row>
+        
+        <Row>
+          <Button variant="light">Light</Button>{' '}
+          <Button variant="dark">Dark</Button>{' '}
+          <Button variant="link">Link</Button>{' '}
+        </Row>
 
         <Navbar />
+        </Container>
+       </div>
 
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,30 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-const name = <h1> Daniel Hartley </h1>
-const googleImg = <img alt='chaf' src="C:\Users\Daniel_Hartley\Projects\react\daniel-test-app\src\pictures\AlternateRealityChad.png" />
-
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  googleImg,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
